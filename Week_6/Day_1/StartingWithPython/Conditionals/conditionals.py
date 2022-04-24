8c474561d30bbb9cd2c5537c66fa16419ed35a47
Greedy_Jabberwock <greedy.jabberwock@gmail.com>
2022-04-24 21:01:48 +0300

    Little changes

--- Week_6/Day_1/StartingWithPython/Conditionals/conditionals.py
+++ Week_6/Day_1/StartingWithPython/Conditionals/conditionals.py
@@ -1,4 +1,4 @@
-#===========================FizzBuzz Exercise===================================
+# ===========================FizzBuzz Exercise===================================
 
 userInput = int(input('Print a number between 1 and 100: '))
 dividedBy5 = userInput % 5 == 0
@@ -6,11 +6,13 @@ dividedBy3 = userInput % 3 == 0
 fizz = 'Fizz'
 buzz = 'Buzz'
 
-if (dividedBy5 and dividedBy3): 
-    print(fizz+buzz)
-elif (dividedBy5):
-    print(buzz)
-elif (dividedBy3):
-    print(fizz)
-else:
-    print('Not divided by 5 or 3')
+# if dividedBy5 and dividedBy3:
+#     print(fizz, end=buzz)
+# elif dividedBy5:
+#     print(buzz)
+# elif dividedBy3:
+#     print(fizz)
+# else:
+#     print('Not divided by 5 or 3')
+
+print('Fizz' if dividedBy3 else '', end='Buzz' if dividedBy5 else '\n');
--- Week_6/Day_1/StartingWithPython/Variables/start.py
+++ Week_6/Day_1/StartingWithPython/Variables/start.py
@@ -1,23 +1,23 @@
 # ============================================Exercise 1=========================================
 
-cars = 100 #the line creates integer value. Value presents cars amount.
-space_in_a_car = 4.0 ##the line creates integer value. Value presents car's capacity.
-drivers = 30 #the line creates integer value. Value presents amount of drivers.
-passengers = 90 #the line creates integer value. Value presents amount of passengers.
-cars_not_driven = cars - drivers #the line calculates integer value. Value present amount of cars without driver.
-cars_driven = drivers #the line calculates integer value. Value presents how many cars can be driven.
-carpool_capacity = cars_driven * space_in_a_car #the line calculates integer value. Value prenets how many passengers can be transpored.
-average_passengers_per_car = passengers / cars_driven #the line calculates integer value. Value presents average amount of passengers in one car.
-
+cars = 100  # the line creates integer value. Value presents cars amount.
+space_in_a_car = 4.0  # the line creates integer value. Value presents car's capacity.
+drivers = 30  # the line creates integer value. Value presents amount of drivers.
+passengers = 90  # the line creates integer value. Value presents amount of passengers.
+cars_not_driven = cars - drivers  # the line calculates integer value. Value present amount of cars without driver.
+cars_driven = drivers  # the line calculates integer value. Value presents how many cars can be driven.
+carpool_capacity = cars_driven * space_in_a_car  # the line calculates integer value. Value prenets how many  passengers can be transpored.
+average_passengers_per_car = passengers / cars_driven  # the line calculates integer value. Value presents average  amount of passengers in one car.
 
 print(f"There are {cars} cars available.")
 print(f"There are only {drivers} drivers available.")
 print(f"There will be {cars_not_driven} empty cars today.")
 print(f"We can transport {carpool_capacity} people today.")
 print(f"We have {passengers} to carpool today.")
-print(f"We need to put about {average_passengers_per_car} in each car.")
+print(f"We need to put about {average_passengers_per_car}'"
+      "in each car.")
 
 # ============================================Exercise 2=========================================
 
-age = input("How old are you? ") #programm will be waiting user's string value.
-print("You are {} years old".format(age)) #programm will return formatted string with the user's value.
+age = input("How old are you? ")  # program will be waiting user's string value.
+print("You are {} years old".format(age))  # program will return formatted string with the user's value.
