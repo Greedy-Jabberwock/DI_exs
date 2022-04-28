1254bc8d0dd23bc219fa637493cb7e7e22b2fe8c
Greedy_Jabberwock <greedy.jabberwock@gmail.com>
2022-04-28 17:57:01 +0300

    Tic-Tac-Toe changes

--- Week_7/Day_5/MiniProject_TicTacToe/main.py
+++ Week_7/Day_5/MiniProject_TicTacToe/main.py
@@ -1,8 +1,31 @@
+from random import randint
+
 cells = [
     [' ', ' ', ' '],
     [' ', ' ', ' '],
     [' ', ' ', ' ']
 ]
+
+players = {
+    'first_player': {
+        'sign': 'X',
+        'human': False,
+        'coordinates': {
+            'x': None,
+            'y': None
+        }
+    },
+    'second_player': {
+        'sign': 'O',
+        'human': False,
+        'coordinates': {
+            'x': None,
+            'y': None
+        }
+    }
+}
+
+win = False
 
 
 def print_board(cell_values):
@@ -16,8 +39,18 @@ def print_board(cell_values):
     print(border)
 
 
-def get_coordinates(player):
-    pass
+def get_coordinates():
+    for key, value in players.items():
+        if players[key]['human']:
+            x, y = get_player_coordinates
+        else:
+            x, y = get_ai_coordinates
+        players[key]['coordinates']['x'] = x
+        players[key]['coordinates']['y'] = y
+
+
+def check_validation(x_coord, y_coord):
+    return cells[y_coord][x_coord] == ' '
 
 
 def make_turn():
@@ -28,20 +61,33 @@ def check_win():
     pass
 
 
+def get_players_order():
+    valid = False
+    while not valid:
+        user_input = input('Do you want to be "X" or "O"?\n').lower()
+        valid = True
+        if user_input == 'x':
+            players['first_player']['human'] = True
+        elif user_input == 'o':
+            players['second_player']['human'] = True
+        else:
+            valid = False
+
+
 def main():
     # print_board(cells)
     # x, y = get_coordinates(current_player)
     # make_turn(x, y, current_player)
     # check_win(cells)
-    win = False
-    first_player = ''
-    second_player = ''
+
     border = '========================================'
     print(border)
     print("Hello, and welcome to the tic-tac-toe!")
     print(border)
-    player = input('Do you want to be "X" or "O"?')
+    get_players_order()
+    get_coordinates()
+    while not win:
+        pass
 
 
-
-main()
+# main()
