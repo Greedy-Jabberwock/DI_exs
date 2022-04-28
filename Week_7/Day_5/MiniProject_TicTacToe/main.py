d61cf5d13e0ca2c2804a274826cfbeef9d267e90
Greedy_Jabberwock <greedy.jabberwock@gmail.com>
2022-04-28 19:53:09 +0300

    Adding the tic-tac-toe completed.

--- Week_7/Day_5/MiniProject_TicTacToe/main.py
+++ Week_7/Day_5/MiniProject_TicTacToe/main.py
@@ -1,9 +1,11 @@
 from random import randint
 
+border = '========================================'
+empty_cell = '   '
 cells = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' ']
+    [empty_cell, empty_cell, empty_cell],
+    [empty_cell, empty_cell, empty_cell],
+    [empty_cell, empty_cell, empty_cell]
 ]
 
 players = {
@@ -25,40 +27,114 @@ players = {
     }
 }
 
-win = False
 
-
-def print_board(cell_values):
+def print_board():
     border = '-------------'
     print(border)
-    print(f'| {cell_values[0][0]} | {cell_values[0][1]} | {cell_values[0][2]} |')
+    print(f'| {cells[0][0]} | {cells[0][1]} | {cells[0][2]} |')
     print(border)
-    print(f'| {cell_values[1][0]} | {cell_values[1][1]} | {cell_values[1][2]} |')
+    print(f'| {cells[1][0]} | {cells[1][1]} | {cells[1][2]} |')
     print(border)
-    print(f'| {cell_values[2][0]} | {cell_values[2][1]} | {cell_values[2][2]} |')
+    print(f'| {cells[2][0]} | {cells[2][1]} | {cells[2][2]} |')
     print(border)
 
 
-def get_coordinates():
-    for key, value in players.items():
-        if players[key]['human']:
-            x, y = get_player_coordinates
-        else:
-            x, y = get_ai_coordinates
-        players[key]['coordinates']['x'] = x
-        players[key]['coordinates']['y'] = y
+def get_coordinates(player):
+    if player['human']:
+        x, y = get_player_coordinates()
+    else:
+        x, y = get_ai_coordinates()
+    player['coordinates']['x'] = x
+    player['coordinates']['y'] = y
 
 
-def check_validation(x_coord, y_coord):
-    return cells[y_coord][x_coord] == ' '
+def get_player_coordinates():
+    marker = True
+    print('It\'s your turn now!')
+    while marker:
+        player_x, player_y = input('Input x and y coordinates, separated by comma\n').strip().split(',')
+        try:
+            player_x = int(player_x) - 1
+            player_y = int(player_y) - 1
+            if 0 <= player_x <= 2 and 0 <= player_y <= 2:
+                print(cells[player_y][player_x])
+                if cells[player_y][player_x] == empty_cell:
+                    marker = False
+                else:
+                    print('Sorry, this cell is not empty. Try again.')
+                    continue
+            else:
+                print('There are no such coordinates in the grid.')
+                continue
+        except ValueError:
+            print('One or both of values are invalid.')
+            continue
+        print('get_player_coordinates works')
+        return player_x, player_y
 
 
-def make_turn():
-    pass
+def get_ai_coordinates():
+    print('AI turns')
+    ai_x = randint(0, 2)
+    ai_y = randint(0, 2)
+    if cells[ai_y][ai_x] == empty_cell:
+        return ai_x, ai_y
+    else:
+        return get_ai_coordinates()
+
+
+def make_turn(player):
+    x = player['coordinates']['x']
+    y = player['coordinates']['y']
+    cells[y][x] = player['sign']
 
 
 def check_win():
-    pass
+    winner_msg = ' is a winner!'
+    empty_cell_counter = 0
+
+    for item in cells:
+        for element in item:
+            if element != empty_cell:
+                empty_cell_counter += 1
+    if cells[0][0] == cells[1][1] == cells[2][2] and cells[0][0] != empty_cell:
+        print('\\ condition')
+        print(f'{cells[0][0]}{winner_msg}')
+        return True
+    if cells[0][2] == cells[1][1] == cells[2][0] and cells[0][2] != empty_cell:
+        print(f'{cells[0][2]}{winner_msg}')
+        print('/ condition')
+        return True
+    for column in range(len(cells)):
+        if cells[column].count('X') == 3:
+            print('- X condition')
+            print(f'X {winner_msg}')
+            return True
+        if cells[column].count('O') == 3:
+            print('- O condition')
+            print(f'O {winner_msg}')
+            return True
+        count_x = 0
+        count_o = 0
+        for row in range(len(cells)):
+            if cells[row][column] == 'X':
+                count_x += 1
+            elif cells[row][column] == 'O':
+                count_o += 1
+        if count_x == 3:
+            print('| X condition')
+            print(f'X {winner_msg}')
+            return True
+        if count_o == 3:
+            print('| O condition')
+            print(f'O {winner_msg}')
+            return True
+    else:
+        if empty_cell_counter == 9:
+            print('It\'s a tie!')
+            return True
+        else:
+            print('Nobody wins now')
 
 
 def get_players_order():
@@ -75,19 +151,28 @@ def get_players_order():
 
 
 def main():
-    # print_board(cells)
-    # x, y = get_coordinates(current_player)
-    # make_turn(x, y, current_player)
-    # check_win(cells)
-
-    border = '========================================'
     print(border)
     print("Hello, and welcome to the tic-tac-toe!")
     print(border)
     get_players_order()
-    get_coordinates()
-    while not win:
-        pass
+    first = players['first_player']
+    second = players['second_player']
+    print_board()
+
+    while True:
+        get_coordinates(first)
+        print('First player is turning')
+        make_turn(first)
+        print_board()
+        if check_win():
+            break
+        print('Getting coordinates from second player.')
+        get_coordinates(second)
+        print('Second player is making a turn.')
+        make_turn(second)
+        print_board()
+        if check_win():
+            break
 
 
-# main()
+main()
