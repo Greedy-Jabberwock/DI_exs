12f9037a572f3d5e593791a404ec391074a24197
Greedy_Jabberwock <greedy.jabberwock@gmail.com>
2022-05-15 21:02:17 +0300

    Little changes in output

--- Week_8/Day_2/DailyChallenge_MacDonaldFarm/main.py
+++ Week_8/Day_2/DailyChallenge_MacDonaldFarm/main.py
@@ -1,14 +1,10 @@
-from re import A
-
-
 class Farm():
-
     animals_dict = {}
     
     def __init__(self, name):
         self.name = name
     
-    def add_animal(self ,animal_type, count=1):
+    def add_animal(self, animal_type, count=1):
         animals = self.animals_dict
         if animal_type in animals.keys():
             animals[animal_type] += count
@@ -20,21 +16,21 @@ class Farm():
 
     def get_info(self):
         header = "Old McDonald's farm: \n"
-        edges = ('*' * 55) + ('\n')
+        edges = ('*' * 30) + '\n'
         info = ''
         animals = self.animals_dict
-        line_break = f"*{'_' * (len(edges) - 3)}*\n"
+        line_break = f"{'-' * (len(edges) + 3)}\n"
         info += header
         info += edges
         if animals.keys():
             for key, value in animals.items():
                 info_values = f'\t{key:<10}:{value:^15}'
-                spaces = ' ' * (len(edges) - len(info_values) - 9)
-                full_line = f'* {info_values}{spaces}*\n'
+                spaces = ' ' * (round(len(edges) - len(info_values)))
+                full_line = f' {spaces}{info_values}{spaces}\n'
                 info += full_line + line_break
         else:
             false_info = f'There are no animals at the {self.name}\'s farm yet.'
-            false_spaces = ' ' * (len(edges) - len(false_info) - 4)
+            false_spaces = ' ' * (len(edges) - len(false_info) )
             full_false_line = f'* {false_info}{false_spaces}*\n'
             info += full_false_line
         info += edges
@@ -45,12 +41,13 @@ class Farm():
         types = self.get_animal_types()
         last_el = types[len(types) - 1]
         result = f"{self.name}'s farm has "
-        for type in types:
-            type_str = f'{type}' if self.animals_dict[type] == 1 else f'{type}s'
-            result += f'{type_str}, ' if type != last_el else f'and {type_str}'
-        borders = '-' * (len(result) + 4)
-        result = f'{borders}\n| {result} |\n{borders}'
+        for _type in types:
+            type_str = f'{_type}' if self.animals_dict[_type] == 1 else f'{_type}s'
+            result += f'{type_str}, ' if _type != last_el else f'and {type_str}'
+        borders = '-' * (len(result) + 7)
+        result = f'Short info:\n{borders}\n| {result} |\n{borders}'
         return result
+
 
 mcdonald = Farm('McDonald')
 
