37a3e7f21a9d2ad9093216eee598ad3d4c1395c7
Greedy_Jabberwock <greedy.jabberwock@gmail.com>
2022-05-23 11:37:07 +0300

    Rewrite pieces of code according to your reccomendations.

--- Week_8/Day_2/DailyChallenge_MacDonaldFarm/main.py
+++ Week_8/Day_2/DailyChallenge_MacDonaldFarm/main.py
@@ -1,51 +1,40 @@
 class Farm():
-    animals_dict = {} # create it inside the init: self.animals_dict = {} because it's part from the class
-    
+
     def __init__(self, name):
         self.name = name
-    
+        self.animals_dict = {}  # (+)
+
     def add_animal(self, animal_type, count=1):
         animals = self.animals_dict
-        if animal_type in animals.keys(): # you can do instead: if animal_type in animals
+        if animal_type in animals:  # (+)you can do instead: if animal_type in animals
             animals[animal_type] += count
         else:
             animals[animal_type] = count
 
     def get_animal_types(self):
-        return [x for x in self.animals_dict.keys()] # why you do this and not just self.animals_dict.keys()
+        return list(self.animals_dict.keys())  # (+) why you do this and not just self.animals_dict.keys()
+        # ANSWER: because Python raised an error, that dictionary keys are not subscriptable,
+        # i forgot that i just can  use list().
 
-    def get_info(self): # please rerwite this function it's too complicated 
+    def get_info(self):  # (+) please rerwite this function it's too complicated
         header = "Old McDonald's farm: \n"
-        edges = ('*' * 30) + '\n'
-        info = ''
+        line_break = "-" * len(header) + '\n'
+        info = f'{header}{line_break}'
         animals = self.animals_dict
-        line_break = f"{'-' * (len(edges) + 3)}\n"
-        info += header
-        info += edges
         if animals.keys():
             for key, value in animals.items():
-                info_values = f'\t{key:<10}:{value:^15}'
-                spaces = ' ' * (round(len(edges) - len(info_values)))
-                full_line = f' {spaces}{info_values}{spaces}\n'
-                info += full_line + line_break
+                info += f'\t{key:<10}:{value:^15}\n {line_break}'
         else:
-            false_info = f'There are no animals at the {self.name}\'s farm yet.'
-            false_spaces = ' ' * (len(edges) - len(false_info) )
-            full_false_line = f'* {false_info}{false_spaces}*\n'
-            info += full_false_line
-        info += edges
+            info += f'There are no animals at the {self.name}\'s farm yet.'
 
         return info
 
-    def get_short_info(self):  # please rerwite this function it's too complicated 
+    def get_short_info(self):  # (+) please rerwite this function it's too complicated
         types = self.get_animal_types()
-        last_el = types[len(types) - 1]
-        result = f"{self.name}'s farm has "
-        for _type in types:
-            type_str = f'{_type}' if self.animals_dict[_type] == 1 else f'{_type}s'
-            result += f'{type_str}, ' if _type != last_el else f'and {type_str}'
-        borders = '-' * (len(result) + 7)
-        result = f'Short info:\n{borders}\n| {result} |\n{borders}'
+        types = [f'{x}s' for x in types]
+        result = f"| {self.name}'s farm has " + ", ".join(types) + " |"
+        border = '-' * (len(result) + 3)
+        result = f'Short info:\n{border}\n{result}\n{border}'
         return result
 
 
