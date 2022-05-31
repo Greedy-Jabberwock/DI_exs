5b20a5b13891ecd10651f9539bed6b22918cc95f
Greedy_Jabberwock <greedy.jabberwock@gmail.com>
2022-05-31 11:20:25 +0300

    Added changes with the tutors reqiurements

--- Week_9/Day_2/DailyChallenge_Circle/main.py
+++ Week_9/Day_2/DailyChallenge_Circle/main.py
@@ -1,13 +1,18 @@
 from math import pi
 import turtle
 
+
 class Circle:
-    circles = [] # make it private
+    __circles = []  # (+) make it private
 
     def __init__(self, radius):
         self.radius = radius
         self.diameter = radius*2
         Circle.add_to_list(self)
+
+    @property
+    def circles(self):
+        return Circle.__circles
 
     def calculate_area(self):
         return round(pi * self.radius ** 2, 2)
@@ -17,26 +22,26 @@ class Circle:
 
     @staticmethod
     def add_to_list(cls):
-        Circle.circles.append(cls)
+        Circle.__circles.append(cls)
 
     @staticmethod
     def sort_circles():
-        Circle.circles.sort()
+        Circle.__circles.sort()
 
     def __add__(self, other):
         self.radius += other.radius
         return self
 
     def __gt__(self, other):
-        return True if self.radius > other.radius else False # or just return self.radius > other.radius
+        return self.radius > other.radius  # (+) or just return self.radius > other.radius
 
     def __eq__(self, other):
-        return True if self.radius == other.radius else False # or just: self.radius == other.radius
+        return self.radius == other.radius  # (+) or just: self.radius == other.radius
 
     def __repr__(self):
-        return f'Class {self.__class__.__name__}, ' \ # for multiline use """ """
-               f'radius: {self.radius}, ' \
-               f'diameter: {self.diameter}\n'
+        return f"""Class {self.__class__.__name__},  
+               radius: {self.radius},  \
+               diameter: {self.diameter}\n"""  # (+) for multiline use """ """
 
     def __str__(self):
         return f'{self.__class__.__name__} with radius {self.radius}'
@@ -52,4 +57,3 @@ Circle.sort_circles()
 print(Circle.circles)
 print(c1.calculate_area())
 c1.print_circle()
-
