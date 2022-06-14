dbd3211be5328e94890a7045143662f8b8c177b5
Greedy_Jabberwock <greedy.jabberwock@gmail.com>
2022-06-14 20:55:10 +0300

    Week12 Day2 DailyChallenge completed

---
+++ Week_12/Day_4/DailyChallenge_Actors/actors.sql
@@ -0,0 +1,7 @@
+-- SELECT *
+SELECT MAX(actor_id) as actors_total
+FROM actors_exs
+
+-- INSERT INTO actors_exs(first_name, last_name, age, number_oscars)
+-- VALUES (,,,)
+-- Outcome will be an error. 
