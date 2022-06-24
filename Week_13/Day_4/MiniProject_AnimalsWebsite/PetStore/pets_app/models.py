c991c7fc43af19f7632399b3e7b8afc235db8bc8
Greedy_Jabberwock <greedy.jabberwock@gmail.com>
2022-06-24 17:16:26 +0300

    Added Bonus Part I to Week_13/Day_4/AnimalWebsite

--- Week_13/Day_4/MiniProject_AnimalsWebsite/PetStore/pets_app/models.py
+++ Week_13/Day_4/MiniProject_AnimalsWebsite/PetStore/pets_app/models.py
@@ -14,6 +14,7 @@ class Pet(db.Model):
     price = column(db.Integer, nullable=False)
     image = column(db.String, nullable=False)
     cart_id = column(db.Integer, db.ForeignKey('cart.cart_id'))
+    count = column(db.Integer, default=0)
 
     def __repr__(self):
         return f'<Pet: {self.pet_id}, {self.name}, {self.image}>'
@@ -24,7 +25,6 @@ class Cart(db.Model):
 
     cart_id = column(db.Integer, primary_key=True, autoincrement=True)
     pets = db.relationship('Pet', backref='cart')
-    numbers_of_pets = column(db.Integer, default=0)
 
     def __repr__(self):
         return f'<Cart {self.cart_id}, {self.pets}>'
@@ -37,19 +37,29 @@ class Cart(db.Model):
     def add_to_cart(self, pet_id):
         pet = Pet.query.filter_by(pet_id=pet_id).first()
         setattr(pet, 'cart_id', self.cart_id)
-        cart = Cart.query.first()
-        setattr(cart, 'numbers_of_pets', self.numbers_of_pets + 1)
+        setattr(pet, 'count', pet.count + 1)
         db.session.commit()
 
-    def remove_from_cart(self, pet_id):
+    @staticmethod
+    def decrease_count(pet):
+        setattr(pet, 'count', pet.count - 1 if pet.count != 0 else 0)
+
+    def remove_from_cart(self, pet_id, remove=False):
         pet = Pet.query.filter_by(pet_id=pet_id).first()
-        setattr(pet, 'cart_id', None)
+        self.decrease_count(pet)
+        if not pet.count or remove:
+            setattr(pet, 'cart_id', None)
+            setattr(pet, 'count', 0)
         db.session.commit()
 
     def get_all_data(self):
-        return {'id': self.cart_id, 'pets': self.pets}
+        return {'id': self.cart_id, 'pets': self.pets,
+                'total': self.get_total(),
+                'pets_count': self.number_of_pets()}
 
     def get_total(self):
-        return sum([x.price for x in self.pets]) if self.pets else 0
+        return sum([x.price * x.count for x in self.pets]) if self.pets else 0
 
+    def number_of_pets(self):
+        return sum([x.count for x in self.pets]) if self.pets else 0
 
--- Week_13/Day_4/MiniProject_AnimalsWebsite/PetStore/pets_app/routes.py
+++ Week_13/Day_4/MiniProject_AnimalsWebsite/PetStore/pets_app/routes.py
@@ -1,6 +1,8 @@
 from pets_app import app, db
 from flask import render_template, flash, url_for, redirect
 from pets_app.models import Pet, Cart
+import requests
+import json
 
 
 def get_pet(pet_id):
@@ -9,12 +11,15 @@ def get_pet(pet_id):
 
 @app.route('/')
 def home():
+    # responce = requests.get("https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=5").json()
+    # facts = (x['text'] for x in responce)
+    # return render_template('index.html', facts=facts)
     return render_template('index.html')
 
 
 @app.route('/pets')
 def pets():
-    return render_template('pets.html', pets=Pet.query.all())
+    return render_template('pets.html', pets=Pet.query.order_by('pet_id').all())
 
 
 @app.route('/pet/<int:pet_id>')
@@ -24,24 +29,31 @@ def pet(pet_id):
 
 @app.route('/cart')
 def cart():
-    cart = Cart.query.first()
-    return render_template('cart.html', cart=cart)
+    return render_template('cart.html', cart=Cart.query.first())
 
 
 @app.route('/add_to_cart/<int:pet_id>')
 def add_to_cart(pet_id):
     cart = Cart.query.first()
     if not cart:
-        db.session.add(Cart(numbers_of_pets=0))
+        db.session.add(Cart())
         db.session.commit()
     cart.add_to_cart(pet_id)
     flash('Item added to cart!', 'success')
-    return redirect(url_for('cart'))
+    return redirect(url_for('pets'))
 
 
 @app.route('/remove_from_cart/<int:pet_id>')
 def remove(pet_id):
+    Cart.query.first().remove_from_cart(pet_id, remove=True)
+    return redirect(url_for('cart'))
+
+
+@app.route('/change_count/<int:pet_id>/<int:sign>')
+def change_count(pet_id, sign):
     cart = Cart.query.first()
-    if cart:
+    if sign:
+        cart.add_to_cart(pet_id)
+    else:
         cart.remove_from_cart(pet_id)
     return redirect(url_for('cart'))
