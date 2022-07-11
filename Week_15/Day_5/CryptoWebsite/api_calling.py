040f9caaa1c0af15b8766e21ca09ba121ca70140
Ayal Ganem <ayalganem55@gmail.com>
2022-07-11 14:40:03 +0300

    Update api_calling.py

--- Week_15/Day_5/CryptoWebsite/api_calling.py
+++ Week_15/Day_5/CryptoWebsite/api_calling.py
@@ -5,7 +5,7 @@ import json
 
 
 def populate():
-    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map' # you can export all the constans to sperate file called consatns 
     parameters = {
         'listing_status': 'active',
         'limit': '20',
@@ -14,7 +14,7 @@ def populate():
 
     headers = {
         'Accepts': 'application/json',
-        'X-CMC_PRO_API_KEY': '15daef1e-3fcc-46e5-8dcb-694f950ab633',
+        'X-CMC_PRO_API_KEY': '15daef1e-3fcc-46e5-8dcb-694f950ab633', # this value you can export to environment variable
     }
 
     session = Session()
